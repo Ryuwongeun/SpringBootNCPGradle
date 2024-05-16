436340deabdcad6b9f80c436d70d0ca871b14196
Ryuwongeun <wongeun0311@gmail.com>
2024-05-16 14:33:05 +0900

    1

---
+++ src/main/webapp/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Spring Boot + React + NCP</h1>
+      <img src='./image/sb.jpg' />
+    </div>
+  );
+};
+
+export default App;
